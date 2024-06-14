4852be83665a1587ff06dfe388585460d6a45de0
laraesquivel <laraesquivelsantos@gmail.com>
2024-06-14 08:15:11 -0300

    Ok

--- src/index.py
+++ src/index.py
@@ -1,7 +1,3 @@
-import asyncio
-from motor.motor_asyncio import AsyncIOMotorClient
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.interval import IntervalTrigger
 import os
 from dotenv import load_dotenv
 
