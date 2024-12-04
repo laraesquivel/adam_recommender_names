232c472082271f721a3e411ff264998cddf41604
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2024-12-04 14:43:31 -0300

    att

--- src/recommender_system/new_cold_start.py
+++ src/recommender_system/new_cold_start.py
@@ -18,32 +18,39 @@ class NewColdStart:
 
         # Conecção com as coleções para serem processadas
         names_collection = babynames_db['names']
-        brazilian_names_collection = babynames_db['brazilian_names']
+        brazilian_names_collection = babynames_db['brazilianNames']
 
         # Selecionando o array de nomes para refazer a busca fria
         names_doc = names_collection.find({}, {"name": 1, "recommendedNames": 1})
+
+        # Dicionário para guardar o nome e seus novos nomes recomendados para salvar em um arquivo JSON
+        names_with_new_cold_start = {}
 
         # Percorrendo as linhas da coleção para depois percorrer o array de nomes recomendados
         for line in names_doc:
             name = line.get('name') # nome principal
             recommended_names = line.get('recommendedNames', []) # array de nomes recomendados
 
-            print(f"Name: {name}")
+            #print(f"Name: {name}")
 
             # Criar um array para colocar somente os nomes recomendados que estão na coleção de nomes brasileiros
             recommended_names_in_brazilian_names = []
 
             for recomended_name in recommended_names:
-                print(f"Recomended Name: {recomended_name}")
+                #print(f"Recomended Name: {recomended_name}")
 
                 # Buscando o nome recomendado na coleção de nomes brasileiros
-                brazilian_name_doc = brazilian_names_collection.find_one({"name": recomended_name})
+                brazilian_name_doc = brazilian_names_collection.find_one({"nome_x": recomended_name})
 
                 # Se o nome for encontrado, ele é adicionado na nova recomendação da busca fria
                 if brazilian_name_doc is not None:
+                    # Verificando se o gênero do nome recomendado é igual ao gênero do nome principal
+                    #if brazilian_name_doc.get('genero')[0] == line.get('gender'):
                     recommended_names_in_brazilian_names.append(recomended_name)
             
-            ## IR SALVANDO EM UM ARQUIVO JSON PARA IR VISUALIZANDO O PROCESSAMENTO DOS DADOS
+            #names_with_new_cold_start[name] = recommended_names_in_brazilian_names
+
+            # Salvar os dados da nova busca fria em um arquivo JSON
             with open('new_cold_start.json', 'a') as file:
                 json.dump({name: recommended_names_in_brazilian_names}, file)
                 file.write('\n')
@@ -55,3 +62,4 @@ class NewColdStart:
 
 
 NewColdStart.set_URI('mongodb+srv://laraesquivel:OVyyiX5pIMj4vthh@babys.iuiuuvp.mongodb.net/')
+NewColdStart.process_collections()
