b3351310575d0ade65c411df0d93add5bda79804
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2024-12-05 16:50:04 -0300

    cold start: second part

--- src/recommender_system/new_cold_start.py
+++ src/recommender_system/new_cold_start.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import pandas as pd
 import random
 import json
 
@@ -20,8 +21,11 @@ class NewColdStart:
         names_collection = babynames_db['names']
         brazilian_names_collection = babynames_db['brazilianNames']
 
+        # Carregando o arquivo com os nomes mais populares por década
+        popular_names = pd.read_csv('src/brazilian_names_processing/nomes_populares por_decada.csv')
+
         # Selecionando o array de nomes para refazer a busca fria
-        names_doc = names_collection.find({}, {"name": 1, "recommendedNames": 1})
+        names_doc = names_collection.find({}, {"name": 1, "recommendedNames": 1, "gender": 1})
 
         # Dicionário para guardar o nome e seus novos nomes recomendados para salvar em um arquivo JSON
         names_with_new_cold_start = {}
@@ -30,6 +34,7 @@ class NewColdStart:
         for line in names_doc:
             name = line.get('name') # nome principal
             recommended_names = line.get('recommendedNames', []) # array de nomes recomendados
+            genero = line.get('gender') # gênero do nome principal
 
             # Criar um array para colocar somente os nomes recomendados que estão na coleção de nomes brasileiros
             recommended_names_in_brazilian_names = []
@@ -41,12 +46,21 @@ class NewColdStart:
 
                 # Se o nome for encontrado, ele é adicionado na nova recomendação da busca fria
                 if brazilian_name_doc is not None:
-
+                    
                     # Verificando se o gênero do nome recomendado é igual ao gênero do nome principal
-                    if brazilian_name_doc.get('gender') == line.get('gender'):
+                    if brazilian_name_doc.get('gender') == genero or genero == 'U':
                         recommended_names_in_brazilian_names.append(recomended_name)
             
-            #names_with_new_cold_start[name] = recommended_names_in_brazilian_names
+            #names_with_new_cold_start[name] = recommended_names_in_brazilian_names]
+
+            # Preencher as listas de nomes recomendados ue faltam nomes com a lista de nomes mais populares
+            while len(recommended_names_in_brazilian_names) < 10:
+                if genero == 'F':
+                    # random_name = random.choice(popular_names[''].values)
+                    # if random_name not in recommended_names_in_brazilian_names:
+                    #     recommended_names_in_brazilian_names.append(random_name)
+                elif genero == 'M':
+                
 
             # Salvar os dados da nova busca fria em um arquivo JSON
             with open('new_cold_start.json', 'a') as file:
