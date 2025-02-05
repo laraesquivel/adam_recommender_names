4952a22433eec503a4788bfc742702f614e0bb93
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2025-02-05 15:27:31 -0300

    remaking cold start

--- src/recommender_system/new_cold_start.py
+++ src/recommender_system/new_cold_start.py
@@ -188,9 +188,105 @@ class NewColdStart:
 
         # Fecha a conexão e o arquivo
         client.close()
-        file.close()        
+        file.close()
+
+    @classmethod
+    def cold_start(cls):
+
+        # Método final para realizar a busca fria dos nomes, pois as outras duas funçoes anteriores já foram executadas
+
+        # Conectando com o MongoDB
+        client = MongoClient(cls.URI)
+        babynames_db = client['babynames']
+
+        # Conecção com as coleções para serem processadas
+        names_collection = babynames_db['names']
+        brazilian_names_collection = babynames_db['brazilianNames']
+        new_names = babynames_db['newNames']
+
+        # Carregando o arquivo com os nomes mais populares por década
+        popular_names = pd.read_csv('src/brazilian_names_processing/nomes_populares por_decada.csv')
+
+        for name_doc in new_names.find():
+            try:
+                name = name_doc.get('name') # nome principal
+                print(name)
+                recommended_names = name_doc.get('recommendedNames', []) # array de nomes recomendados
+                genero = name_doc.get('gender') # gênero do nome principal
+
+                # Criar um array para colocar somente os nomes recomendados que estão na coleção de nomes brasileiros
+                new_rec_names = []
+
+                for recomended_name in recommended_names:
+
+                    # Buscando o nome recomendado na coleção de nomes geral
+                    recommended_name_doc = new_names.find_one({"name": recomended_name})
+
+                    # Se o nome for encontrado, ele é adicionado na nova recomendação da busca fria
+                    if recommended_name_doc is not None:
+                        
+                        # Verificando se o gênero do nome recomendado é igual ao gênero do nome principal
+                        if recommended_name_doc.get('gender') == genero or genero == 'U':
+                            new_rec_names.append(recomended_name)
+
+                # Preencher as listas de nomes recomendados que faltam nomes com a lista de nomes mais populares
+                if genero == 'F': # Se o gênero do nome principal for feminino
+                    while len(new_rec_names) < 10:
+                        random_name_F = random.choice(popular_names['FEMININOS'].values)
+                        name_check = new_names.find_one({"name": random_name_F})
+
+                        # Se o nome não estiver na lista de nomes recomendados e estiver na coleção de nomes
+                        if random_name_F not in new_rec_names and name_check is not None:
+                            new_rec_names.append(random_name_F)
+                        else:
+                            continue
+                elif genero == 'M': # Se o gênero do nome principal for masculino
+                    while len(new_rec_names) < 10:
+                        random_name_M = random.choice(popular_names['MASCULINOS'].values)
+                        name_check = new_names.find_one({"name": random_name_M})
+
+                        if random_name_M not in new_rec_names and name_check is not None:
+                            new_rec_names.append(random_name_M)
+                        else:
+                            continue
+                else: # Se o genero do nome principal for unissex
+                    i = 0 
+                    while len(new_rec_names) < 10:
+
+                        if i % 2 == 0:
+                            random_name_U = random.choice(popular_names['FEMININOS'].values)
+                        else:
+                            random_name_U = random.choice(popular_names['MASCULINOS'].values)
+                        
+                        name_check = new_names.find_one({"name": random_name_U})
+
+                        if random_name_U not in new_rec_names and name_check is not None:
+                            new_rec_names.append(random_name_U)
+                        else:
+                            continue
+                        i += 1
+
+                # Atualizar no MongoDB colocando a lista de nomes recomendados na nova tabela de nomes
+                try:
+                    new_names.update_one(
+                        {"name": name},
+                        {"$set": {"recommendedNames": new_rec_names}},
+                        upsert=True
+                    )
+                except Exception as e:
+                    print(f"Erro ao atualizar o nome '{name}' no banco: {e}")
+
+                # Salvar os dados da nova busca fria em um arquivo JSON
+                with open('cold_start.json', 'a', encoding="utf-8") as file:
+                    json.dump({name: new_rec_names}, file, ensure_ascii=False, indent=4)
+                    file.write('\n')
+
+            except Exception as e:
+                print(f"Erro ao processar o nome '{name}': {e}")
+
 
 
 NewColdStart.set_URI('mongodb+srv://laraesquivel:OVyyiX5pIMj4vthh@babys.iuiuuvp.mongodb.net/')
 # NewColdStart.process_collections()
-NewColdStart.brazilian_names_cold_start()
+# NewColdStart.brazilian_names_cold_start()
+NewColdStart.cold_start()
