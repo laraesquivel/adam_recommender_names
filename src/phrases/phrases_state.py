e704f94d8674f86715b8e98e5b4bf469ed09a44e
Nathielle Alves <nalves@ecomp.uefs.br>
2025-07-15 13:23:12 -0300

    Update phrases_state.py

--- src/phrases/phrases_state.py
+++ src/phrases/phrases_state.py
@@ -37,77 +37,81 @@ class Phrases_State:
         end_of_year = datetime(today.year +1,1,1)
         results = None
 
-        for doc in phrases.find({}):
-            frase = doc['Frase'].lower()
-            time = ('hoje' in frase or 'semana' in frase or 'ano' in frase)
+        try:
 
-            if 'região' in frase and 'origem' in frase and time:
-                for palavra,classificado in cls.ALL_BRAZIL_REGION:
-                    if classificado in frase:
-                        for palavraO, classificadoO in cls.WORDS_ORIGIN:
-                            if palavraO in frase:
-                                if 'hoje' in frase:
-                                    results = actions.aggregate(get_region_origin.pipeline(classificadoO,begin_of_day,end_of_day))
-                                elif 'semana' in frase:
-                                    results = actions.aggregate(get_region_origin.pipeline(classificadoO,begin_of_week,end_of_week))
-                                else:
-                                    results = actions.aggregate(get_region_origin.pipeline(classificadoO,begin_of_year,end_of_year))
-            elif 'região' in frase and 'origem' in frase:
-                for palavra,classificado in cls.ALL_BRAZIL_REGION:
-                    if classificado in frase:
-                        for palavraO, classificadoO in frase:
-                            if palavraO in frase:
-                                results = actions.aggregate(get_region_origin.pipeline(classificado))
-            elif 'região' in frase and time:
-                for palavra, classificado in cls.ALL_BRAZIL_REGION:
-                    if palavra in frase:
-                        if 'hoje' in frase:
-                            results = actions.aggregate(get_region.pipeline(classificado,begin_of_day,end_of_day))
-                        elif 'semana' in frase:
-                            results = actions.aggregate(get_region.pipeline(classificado,begin_of_week,end_of_week))
-                        else:
-                            results = actions.aggregate(get_region.pipeline(classificado,begin_of_day,end_of_day))
-            elif 'origem' in frase and time:
-                for palavra,classificado in cls.WORDS_ORIGIN:
-                    if palavra in frase:
-                        if 'hoje' in frase:
-                            results = actions.aggregate(get_origins.pipeline(classificado,begin_of_day,end_of_day))
-                        elif 'semana' in frase:
-                            results = actions.aggregate(get_origins.pipeline(classificado,begin_of_week,end_of_week))
-                        else:
-                            results = actions.aggregate(get_origins.pipeline(classificado,begin_of_year,end_of_year))
-            elif 'região' in frase:
-                for palavra, classificado in cls.ALL_BRAZIL_REGION:
-                    if palavra in frase:
-                        results = actions.aggregate(get_region.pipeline(classificado))
-            elif 'origem' in frase:
-                for palavra, classificado in cls.WORDS_ORIGIN:
-                    if palavra in frase:
-                        results = actions.aggregate(get_origins.pipeline(classificado))
-            elif time:
-                if 'hoje' in frase:
-                    results = actions.find({'timestamp' : {'$gte' : begin_of_day, '$lt' : end_of_day}})
-                elif 'semana' in frase:
-                    results = actions.find({'timestamp' : {'$gte' : begin_of_week, '$lt' : end_of_week}})
+            for doc in phrases.find({}):
+                frase = doc['Frase'].lower()
+                time = ('hoje' in frase or 'semana' in frase or 'ano' in frase)
+
+                if 'região' in frase and 'origem' in frase and time:
+                    for palavra,classificado in cls.ALL_BRAZIL_REGION:
+                        if classificado in frase:
+                            for palavraO, classificadoO in cls.WORDS_ORIGIN:
+                                if palavraO in frase:
+                                    if 'hoje' in frase:
+                                        results = actions.aggregate(get_region_origin.pipeline(classificadoO,begin_of_day,end_of_day))
+                                    elif 'semana' in frase:
+                                        results = actions.aggregate(get_region_origin.pipeline(classificadoO,begin_of_week,end_of_week))
+                                    else:
+                                        results = actions.aggregate(get_region_origin.pipeline(classificadoO,begin_of_year,end_of_year))
+                elif 'região' in frase and 'origem' in frase:
+                    for palavra,classificado in cls.ALL_BRAZIL_REGION:
+                        if classificado in frase:
+                            for palavraO, classificadoO in frase:
+                                if palavraO in frase:
+                                    results = actions.aggregate(get_region_origin.pipeline(classificado))
+                elif 'região' in frase and time:
+                    for palavra, classificado in cls.ALL_BRAZIL_REGION:
+                        if palavra in frase:
+                            if 'hoje' in frase:
+                                results = actions.aggregate(get_region.pipeline(classificado,begin_of_day,end_of_day))
+                            elif 'semana' in frase:
+                                results = actions.aggregate(get_region.pipeline(classificado,begin_of_week,end_of_week))
+                            else:
+                                results = actions.aggregate(get_region.pipeline(classificado,begin_of_day,end_of_day))
+                elif 'origem' in frase and time:
+                    for palavra,classificado in cls.WORDS_ORIGIN:
+                        if palavra in frase:
+                            if 'hoje' in frase:
+                                results = actions.aggregate(get_origins.pipeline(classificado,begin_of_day,end_of_day))
+                            elif 'semana' in frase:
+                                results = actions.aggregate(get_origins.pipeline(classificado,begin_of_week,end_of_week))
+                            else:
+                                results = actions.aggregate(get_origins.pipeline(classificado,begin_of_year,end_of_year))
+                elif 'região' in frase:
+                    for palavra, classificado in cls.ALL_BRAZIL_REGION:
+                        if palavra in frase:
+                            results = actions.aggregate(get_region.pipeline(classificado))
+                elif 'origem' in frase:
+                    for palavra, classificado in cls.WORDS_ORIGIN:
+                        if palavra in frase:
+                            results = actions.aggregate(get_origins.pipeline(classificado))
+                elif time:
+                    if 'hoje' in frase:
+                        results = actions.find({'timestamp' : {'$gte' : begin_of_day, '$lt' : end_of_day}})
+                    elif 'semana' in frase:
+                        results = actions.find({'timestamp' : {'$gte' : begin_of_week, '$lt' : end_of_week}})
+                    else:
+                        results = actions.find({'timestamp' : {'$gte' : begin_of_year, '$lt' : end_of_year}})
                 else:
-                    results = actions.find({'timestamp' : {'$gte' : begin_of_year, '$lt' : end_of_year}})
-            else:
-                results = actions.find({})
+                    results = actions.find({})
 
-            names = Counter()
-            for action in results:
-                if 'name' in action:
-                    names[action['name']]+=1
-                if 'relationalName' in action:
-                    names[action['relationalName']] += 1
-            top10 = [top_name[0] for top_name in names.most_common(10)]
-            phrases.update_one(
-                {'Frase' : doc['Frase']},
-                {
-                    '$set' : {
-                        'associedNames' : top10
+                names = Counter()
+                for action in results:
+                    if 'name' in action:
+                        names[action['name']]+=1
+                    if 'relationalName' in action:
+                        names[action['relationalName']] += 1
+                top10 = [top_name[0] for top_name in names.most_common(10)]
+                phrases.update_one(
+                    {'Frase' : doc['Frase']},
+                    {
+                        '$set' : {
+                            'associedNames' : top10
+                        }
                     }
-                }
-            )                
+                )
+        except Exception as e:
+            print(f"Erro ao processar as frases: {e}")                
 
 Phrases_State.workflow()
